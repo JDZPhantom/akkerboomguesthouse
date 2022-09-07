72a0592113db304a63282ef80b1fc1cbd60e7bb0
JDZPhantom <51746894+JDZPhantom@users.noreply.github.com>
2022-09-07 15:34:44 +0200

    Home Page UI (redirects to leave a review)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:akkerboomguesthouseapp/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:emoji_feedback/emoji_feedback.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:akkerboomguesthouseapp/customer _review_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,41 +14,168 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
+  Widget _selectedPlan(Color color, String title, String subtitle) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      padding: EdgeInsets.only(left: 20),
+      height: 120,
+      width: 240,
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(15.0)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(title, style: GoogleFonts.bebasNeue(fontSize: 30)),
+          SizedBox(height: 5),
+          Text(
+            subtitle,
+            style: GoogleFonts.bebasNeue(fontSize: 15),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _mainMenu(
+    String image,
+    String name,
+
+  ) {
+    return InkWell(
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(color: Colors.grey, width: 2)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+
+              height: 60,
+              decoration:
+                  BoxDecoration(image: DecorationImage(image: AssetImage(image))),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              name,
+              style: GoogleFonts.bebasNeue(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+      onTap: (){
+              if(name=="Book Now"){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>LoginPage()),
+                );
+              }
+              if(name=="Room Service"){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>LoginPage()),
+                );
+              }
+              if(name=="Leave a Review"){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>Customer_Review()),
+                );
+              }
+              if(name=="Room Cleaning"){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>LoginPage()),
+                );
+              }
+
+
+
+      },
+    );
+  }
 
   get index => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Rate your visit")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "How did you like your stay?",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      backgroundColor: Colors.lime,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.lime,
+        title: Text(
+          "Welcome:",
+          style: TextStyle(fontSize: 20, color: Colors.black87),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Container(
+          height: 800,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30),
+                topLeft: Radius.circular(30),
+              )),
+          child: ListView(
+            padding: EdgeInsets.only(left: 30, top: 30),
+            children: [
+              Text(
+                "Daily Specials: ",
+                style: TextStyle(
+                    fontSize: 19,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w500),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: EmojiFeedback(onChange: (index){print(index);}),
-            ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 30, top: 30),
+                  child: Row(
+                    children: [
+                      _selectedPlan(Colors.lightGreen, "10% discount for couples", "4 couples max"),
+                      _selectedPlan(Colors.lightGreen, "Wagyu Beef Burgers in the kitchen", "selected stock")
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 30,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Select: ',
+                      style: GoogleFonts.bebasNeue(fontSize: 30),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Container(
+                        height: 300,
+                        child: GridView.count(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 8,
+                          childAspectRatio: 1.30,
+                          children: [
+                            _mainMenu('assets/Akkerboom.png', 'Book Now'),
+                            _mainMenu('assets/Akkerboom.png', 'Room Service'),
+                            _mainMenu('assets/Akkerboom.png', 'Room Cleaning'),
+                            _mainMenu('assets/Akkerboom.png', 'Leave a Review')
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
-            Spacer(),
-
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: MaterialButton(onPressed: (){
-                 FirebaseAuth.instance.signOut();
-              }
-                 ,color: Colors.limeAccent,
-                child: Text("sign out"),
-               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
