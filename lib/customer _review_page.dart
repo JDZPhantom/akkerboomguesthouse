a19641669129a86bc2b13d83fdd5a7087d9f1d75
JDZPhantom <51746894+JDZPhantom@users.noreply.github.com>
2022-09-07 16:22:58 +0200

    customer review w backend

--- lib/customer _review_page.dart
+++ lib/customer _review_page.dart
@@ -1,7 +1,17 @@
 import 'package:emoji_feedback/emoji_feedback.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'main.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class Customer_Review extends StatefulWidget {
   const Customer_Review({Key? key}) : super(key: key);
@@ -11,10 +21,34 @@ class Customer_Review extends StatefulWidget {
 }
 
 class _Customer_ReviewState extends State<Customer_Review> {
+  final fb = FirebaseDatabase.instance;
+  final myController = TextEditingController();
+  final feedback = "feedback";
+  late String title;
+  String text = "No Value Entered";
+
+  void _submitText() {
+    setState(() {
+      text = title;
+      FirebaseFirestore.instance
+          .collection('customerReviews')
+          .add({'Review': text});
+    });
+    text="";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Rate your visit")),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.lime,
+        title: Text(
+          "Leave a Review",
+          style: TextStyle(fontSize: 20, color: Colors.black87),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -29,17 +63,40 @@ class _Customer_ReviewState extends State<Customer_Review> {
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: EmojiFeedback(onChange: (index){print(index);}),
+              child: EmojiFeedback(onChange: (index) {
+                print(index);
+              }),
             ),
+            Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                      decoration: InputDecoration(labelText: "Any Feedback?"),
+                      onChanged: (value) => title = value,
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: _submitText,
+                      style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(8),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green),
+                      ),
+                      child: const Text('Submit')),
 
+                ],
+              ),
+            ),
             Spacer(),
-
             Padding(
               padding: const EdgeInsets.only(bottom: 20),
-              child: MaterialButton(onPressed: (){
-                FirebaseAuth.instance.signOut();
-              }
-                ,color: Colors.limeAccent,
+              child: MaterialButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                color: Colors.limeAccent,
                 child: Text("sign out"),
               ),
             ),
